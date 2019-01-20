c32d651fba9bcb11f4cdc4c6ff3b525d91c0cadc
kyubisation <lukas.spirig@gmail.com>
2019-01-20 23:55:29 +0100

    Merge rollup configs

--- rollup.config.js
+++ rollup.config.js
@@ -3,32 +3,18 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import json from 'rollup-plugin-json';
 
+const config = {
+  input: './src/index.ts',
+  external: ['child_process', 'events', 'fs', 'os', 'path', 'util', 'typescript'],
+  plugins: [
+    resolve(),
+    json(),
+    commonjs(),
+    typescript(),
+  ],
+};
+
 export default [
-  {
-    input: './src/index.ts',
-    output: {
-      file: 'index.js',
-      format: 'esm',
-    },
-    external: ['child_process', 'events', 'fs', 'os', 'path', 'util', 'typescript'],
-    plugins: [
-      resolve(),
-      json(),
-      commonjs(),
-      typescript(),
-    ],
-  },
-  {
-    input: './src/index.ts',
-    output: {
-      file: 'main.js',
-      format: 'cjs',
-    },
-    external: ['child_process', 'events', 'fs', 'os', 'path', 'util', 'typescript'],
-    plugins: [
-      resolve(),
-      json(),
-      commonjs(),
-      typescript(),
-    ],
-  }]
+  Object.assign({ output: { file: 'index.js', format: 'esm' } }, config),
+  Object.assign({ output: { file: 'main.js', format: 'cjs' } }, config),
+]
