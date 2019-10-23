4fdc2a096b23f3a6921a76f270cdd18f2422f9df
Lukas Spirig <lukas.spirig@gmail.com>
2019-10-23 21:01:37 +0200

    feat: add uncompressed binaries

--- cli/build.sh
+++ cli/build.sh
@@ -4,16 +4,20 @@ cd /src
 
 echo "Building for Windows 32-bit"
 env GOOS=windows GOARCH=386 go build -ldflags="-s -w -X main.CliVersion=$VERSION" -o /dist/ngssc_32bit.exe main.go
+cp /dist/ngssc_32bit.exe /dist/ngssc_uncompressed_32bit.exe
 upx --brute /dist/ngssc_32bit.exe
 
 echo "Building for Windows 64-bit"
 env GOOS=windows GOARCH=amd64 go build -ldflags="-s -w -X main.CliVersion=$VERSION" -o /dist/ngssc_64bit.exe main.go
+cp /dist/ngssc_64bit.exe /dist/ngssc_uncompressed_64bit.exe
 upx --brute /dist/ngssc_64bit.exe
 
 echo "Building for Linux 32-bit"
 env GOOS=linux GOARCH=386 go build -ldflags="-s -w -X main.CliVersion=$VERSION" -o /dist/ngssc_32bit main.go
+cp /dist/ngssc_32bit /dist/ngssc_uncompressed_32bit
 upx --brute /dist/ngssc_32bit
 
 echo "Building for Linux 64-bit"
 env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w -X main.CliVersion=$VERSION" -o /dist/ngssc_64bit main.go
+cp /dist/ngssc_64bit /dist/ngssc_uncompressed_64bit
 upx --brute /dist/ngssc_64bit
