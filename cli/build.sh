05619218bf7c748ebfefa94b27d74c9c5fe89a23
kyubisation <lukas.spirig@gmail.com>
2020-04-04 17:53:23 +0200

    feat: add ngssc binaries for OSX (darwin)

--- cli/build.sh
+++ cli/build.sh
@@ -21,3 +21,13 @@ echo "Building for Linux 64-bit"
 env GOOS=linux GOARCH=amd64 go build -ldflags="-s -w -X main.CliVersion=$VERSION" -o /dist/ngssc_64bit main.go
 cp /dist/ngssc_64bit /dist/ngssc_64bit_min
 upx --brute /dist/ngssc_64bit_min
+
+echo "Building for Darwin 32-bit"
+env GOOS=darwin GOARCH=386 go build -ldflags="-s -w -X main.CliVersion=$VERSION" -o /dist/ngssc_darwin_32bit main.go
+cp /dist/ngssc_darwin_32bit /dist/ngssc_darwin_32bit_min
+upx --brute /dist/ngssc_darwin_32bit_min
+
+echo "Building for Darwin 64-bit"
+env GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w -X main.CliVersion=$VERSION" -o /dist/ngssc_darwin_64bit main.go
+cp /dist/ngssc_darwin_64bit /dist/ngssc_darwin_64bit_min
+upx --brute /dist/ngssc_darwin_64bit_min
